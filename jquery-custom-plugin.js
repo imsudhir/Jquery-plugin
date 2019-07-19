b080e713124bacd11b1582b957ec4272ea08a060
sudhirsingh111 <46950151+sudhirsingh111@users.noreply.github.com>
2019-07-19 18:37:01 +0530

    updated code

--- jquery-custom-plugin.js
+++ jquery-custom-plugin.js
@@ -1,31 +1,67 @@
- $.fn.greenify = function(options) {
-
-      // JS Oject
-      // var set = {
-      //    name:"sumesh",
-      //    net:function(){
-      //       console.log(this.name);
-      //    }
-      // };
-      // set.net();
  
-      console.log(typeof set);
+
+(function( $ ) {
+
+ $.fn.greenify = function(options) {
+   
+      // JS Oject
+      var set = {
+            name:"sudhir",
+            leftcenter:"{bottom: 0; left: 0;}",
+            rightcenter:"sudhirsingh",
+            bottomleft:"sudhirsingh",
+            bottomright:"sudhirsingh",
+            zxcm:function(){
+            // alert("sdfghjkl");
+              
+
+          },
+         net:function(){
+            console.log(this.name);
+         }
+      };
+
+
+      
+      // set.net();
+      set.zxcm();
+ 
+      console.log(options);
 
       var settings = $.extend({
-         color: "#556b2f"
-      }, options );
+               color : "#556b2f",
+               setposition : "bottomright",
+               addplate:"true"
 
-     var butttn = '<div class="check-button"><label class="switch"><input type="checkbox"> <span class="slider round"></span></label></div>';
-     $(this).append(butttn);
-     $('.check-button input[type="checkbox"]').click(function(){
-            if($(this).prop("checked") == true){
-                $("body").css( "background-color", settings.color );
+      }, options );
+         //  var pos = settings.position();
+         console.log(settings.topleftcenter);
+         var butttn = '<div id="mybutton" onclick="addplateFunction()" class="topleftcenter"><label class="switch"><input type="checkbox"> <span class="slider round"></span></label></div>';
+         $(this).append(butttn);
+         $("#mybutton").removeClass("topleftcenter").addClass(settings.setposition);
+         console.log(settings.addplate);
+         if(settings.addplate=='true'){
+   
+
+         }
+         $('#mybutton input[type="checkbox"]').click(function(){
+         if($(this).prop("checked") == true){
+         $("body1").css( "background-color", settings.color );
             }
             else if($(this).prop("checked") == false){
                $("body").css( "background-color", "" );
-
             }
         });
      return this;
  };
-}(jQuery));
+}(jQuery)); 
+
+jQuery( "body" ).greenify({
+   color:"orange",
+   setposition : "bottomleft",
+   addplate:"true"
+   }); 
+
+function addplateFunction() {
+  document.getElementById("mybutton").innerHTML = '<form id="form"><main style="z-index:999; background-color:black !important; padding: 5px;"><div class="colors-row"><label style="color:white;">Colors:</label><div class="colors-box"><label><input type="radio" name="color" value="#fac563"><div class="color" style="background:#fac563;"></div></label><label><input type="radio" name="color" value="#fac563"><div class="color" style="background:#fac563;"></div></label><label><input type="radio" name="color" value="#ff8100"><div class="color" style="background:#ff8100;"></div></label><label><input type="radio" name="color" value="#ff9329"><div class="color" style="background:#ff9329;"></div></label><label><input type="radio" name="color" value="#ffa500"><div class="color" style="background:#ffa500;"></div></label><label><input type="radio" name="color" value="#f1b174"><div class="color" style="background:#f1b174;"></div></label><label><input type="radio" name="color" value="#f55353"><div class="color" style="background:#f55353;"></div></label><label><input type="radio" name="color" value="#cc33ff"><div class="color" style="background:#cc33ff;"></div></label><label><input type="radio" name="color" value="#fae591"><div class="color" style="background:#fae591;"></div></label><label><input type="radio" name="color" value="#fae591"><div class="color" style="background:#fae591;"></div></label><label><input type="radio" name="color" value="#fae591"><div class="color" style="background:#fae591;"></div></label><label><input type="radio" name="color" value="#fae591"><div class="color" style="background:#fae591;"></div></label><button id="back">Back</button></main></form>';
+}
