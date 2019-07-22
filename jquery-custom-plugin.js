3ffc76af40d557cc30cc4aad53f712674bd4f755
sudhirsingh111 <46950151+sudhirsingh111@users.noreply.github.com>
2019-07-22 19:07:46 +0530

    Update jquery-custom-plugin.js

--- jquery-custom-plugin.js
+++ jquery-custom-plugin.js
@@ -1,4 +1,3 @@
- 
 
 (function( $ ) {
 
@@ -46,10 +45,10 @@
          }
          $('#mybutton input[type="checkbox"]').click(function(){
          if($(this).prop("checked") == true){
-         $("body1").css( "background-color", settings.color );
+         $("body").css( "background-color", settings.color );
             }
             else if($(this).prop("checked") == false){
-               $("body").css( "background-color", "" );
+               $("body").css( "background-color", "red" );
             }
         });
      return this;
@@ -64,4 +63,10 @@ jQuery( "body" ).greenify({
 
 function addplateFunction() {
   document.getElementById("mybutton").innerHTML = '<form id="form"><main style="z-index:999; background-color:black !important; padding: 5px;"><div class="colors-row"><label style="color:white;">Colors:</label><div class="colors-box"><label><input type="radio" name="color" value="#fac563"><div class="color" style="background:#fac563;"></div></label><label><input type="radio" name="color" value="#fac563"><div class="color" style="background:#fac563;"></div></label><label><input type="radio" name="color" value="#ff8100"><div class="color" style="background:#ff8100;"></div></label><label><input type="radio" name="color" value="#ff9329"><div class="color" style="background:#ff9329;"></div></label><label><input type="radio" name="color" value="#ffa500"><div class="color" style="background:#ffa500;"></div></label><label><input type="radio" name="color" value="#f1b174"><div class="color" style="background:#f1b174;"></div></label><label><input type="radio" name="color" value="#f55353"><div class="color" style="background:#f55353;"></div></label><label><input type="radio" name="color" value="#cc33ff"><div class="color" style="background:#cc33ff;"></div></label><label><input type="radio" name="color" value="#fae591"><div class="color" style="background:#fae591;"></div></label><label><input type="radio" name="color" value="#fae591"><div class="color" style="background:#fae591;"></div></label><label><input type="radio" name="color" value="#fae591"><div class="color" style="background:#fae591;"></div></label><label><input type="radio" name="color" value="#fae591"><div class="color" style="background:#fae591;"></div></label><button id="back">Back</button></main></form>';
+
+  jQuery("input[name='color']").click(function(){
+  var colors =jQuery(this).val();
+  jQuery("body").css( "background-color", colors );
+  });
+  
 }
